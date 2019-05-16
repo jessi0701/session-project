4839dcb1c3b35cf8813fb6269afd15ac26a7e770
Kim Nok Hyeong <jessi0701@naver.com>
2019-05-16 05:40:00 +0000

    fix

--- Project/settings.py
+++ Project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,5 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = "accounts.User"
+
+django_heroku.settings(locals())
